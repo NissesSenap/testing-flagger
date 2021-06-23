eb54bb2fc95c091bce54936cfa4b1c1e9ec58315
Edvin Norling <edvin.norling@xenit.se>
2021-06-23 11:26:54 +0200

    Add initial types

--- api/v1alpha1/test_types.go
+++ api/v1alpha1/test_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"github.com/fluxcd/pkg/apis/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,8 +29,78 @@ type TestSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Test. Edit test_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Should the tester start a blocking test or not? Default true
+	// +optional
+	Blocking bool `json:"blocking,omitempty"`
+
+	// Certificate
+	// +optional
+	Certificate *Certificate `json:"certificate,omitempty"`
+
+	// Tekton creates a tekton tester that will check the status of a triggered Tekton PipelineRun.
+	// +optional
+	Tekton *TektonTest `json:"tekton,omitempty"`
+
+	// Webhook creates a webhook tester which will host a endpoint to receive callBack webhooks.
+	// +optional
+	Webhook *WebhookTest `json:"webhook,omitempty"`
+}
+
+type Certificate struct {
+	// SecretRef holds the name to a secret that contains a Kubernetes TLS secret.
+	// +required
+	SecretRef meta.LocalObjectReference `json:"secretRef,omitempty"`
+}
+
+type TektonTest struct {
+	// The namespace where we will find the Tekton PipelineRun, defaults to the namespace the CR is created in.
+	// +optional
+	Namespace string `json:"namespace"`
+
+	// The Tekton eventListener URL
+	// +required
+	Url string `json:"url"`
+}
+
+// +kubebuilder:validation:Enum=grpc;http;http2;tcp
+type PortProtocol string
+
+const (
+	PortProtocolHTTP  PortProtocol = "http"
+	PortProtocolHTTP2 PortProtocol = "http2"
+)
+
+type WebhookTest struct {
+	// content-type header, defaults "application/json".
+	// +optional
+	ContentType string `json:"contentType,omitempty"`
+
+	// Custom headers to add to your webhook
+	// +optional
+	Headers *map[string]string `json:"headers,omitempty"`
+
+	// host header, defaults to the deployment service-name.
+	// +optional
+	Host string `json:"host,omitempty"`
+
+	// What protocol to use when sending the webhook.
+	// Valid values are:
+	// - http (default)
+	// - http2
+	// +optional
+	Protocol PortProtocol `json:"protocol,omitempty"`
+
+	// A token to forward with your webhook.
+	// +optional
+	Token string `json:"token,omitempty"`
+
+	// userAgent header, default "flagger-tester/1.0.0".
+	// +optional
+	UserAgent string `json:"userAgent,omitempty"`
+
+	// Url where to send the webhook
+	// +required
+	Url string `json:"url,omitempty"`
 }
 
 // TestStatus defines the observed state of Test
--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module flagger.app/testing
 go 1.16
 
 require (
+	github.com/fluxcd/pkg/apis/meta v0.10.0
 	github.com/onsi/ginkgo v1.14.1
 	github.com/onsi/gomega v1.10.2
-	k8s.io/apimachinery v0.20.2
+	k8s.io/apimachinery v0.21.1
 	k8s.io/client-go v0.20.2
 	sigs.k8s.io/controller-runtime v0.8.3
 )
